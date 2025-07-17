a85cdb6927ce586699b15184e7bf0bc17718e19e
Ravindra Nath Jha <ravindranathjha76@gmail.com>
2025-07-17 13:53:24 +0530

    Update server configuration to include default app keys and add webhooks settings

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,12 @@
+import { factories } from '@strapi/strapi';
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['defaultKeyA', 'defaultKeyB']),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
